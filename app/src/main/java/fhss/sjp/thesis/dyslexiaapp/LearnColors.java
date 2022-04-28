77f02a6550b8968a990e06651f9d4f6ce41bc3fd
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-04-28 17:02:20 +0530

    Create learn_colors interface

--- app/src/main/java/fhss/sjp/thesis/dyslexiaapp/LearnColors.java
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/LearnColors.java
@@ -4,5 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LearnColors extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.colou);
+    }
+
 
 }
