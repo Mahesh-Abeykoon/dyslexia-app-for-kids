0abc93155420ae760834da7034850ea92c740d79
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-04-28 15:43:20 +0530

    Create MainActivity of the app

---
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/MainActivity.java
@@ -0,0 +1,22 @@
+package fhss.sjp.thesis.dyslexiaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openLearning(View v){
+        Intent intent = new Intent(this,LearnScreen.class);
+        startActivity(intent);
+    }
+}
