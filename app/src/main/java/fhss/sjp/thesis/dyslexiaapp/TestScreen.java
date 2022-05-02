77ca92c28121354b23f4de94ed0653201612ecfe
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-02 19:52:46 +0530

    Create TestScreen navigation

--- app/src/main/java/fhss/sjp/thesis/dyslexiaapp/TestScreen.java
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/TestScreen.java
@@ -1,8 +1,15 @@
 package fhss.sjp.thesis.dyslexiaapp;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class TestScreen extends AppCompatActivity {
 
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_screen);
+    }
+
 }
