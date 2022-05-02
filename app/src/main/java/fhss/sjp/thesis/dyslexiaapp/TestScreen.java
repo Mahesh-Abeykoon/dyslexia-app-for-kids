1804b23342f982900eba695808abac42053956a3
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-02 20:24:46 +0530

    Create Image Identify Screen

--- app/src/main/java/fhss/sjp/thesis/dyslexiaapp/TestScreen.java
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/TestScreen.java
@@ -1,8 +1,11 @@
 package fhss.sjp.thesis.dyslexiaapp;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import fhss.sjp.thesis.dyslexiaapp.examinemenu.ImageIdentify;
 
 public class TestScreen extends AppCompatActivity {
 
@@ -12,4 +15,8 @@ public class TestScreen extends AppCompatActivity {
         setContentView(R.layout.test_screen);
     }
 
+    public void openImageIdentifyScreen(View v){
+        Intent intent = new Intent(this, ImageIdentify.class);
+        startActivity(intent);
+    }
 }
