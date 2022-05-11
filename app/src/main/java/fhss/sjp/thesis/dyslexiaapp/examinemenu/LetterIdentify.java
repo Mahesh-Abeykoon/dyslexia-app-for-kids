2705d598d45bed46491a418f271a8b09e67d1fd9
Mah-eshh <92861@fhss.sjp.ac.lk>
2022-05-11 23:50:06 +0530

    Create an identify letter activity

--- app/src/main/java/fhss/sjp/thesis/dyslexiaapp/examinemenu/LetterIdentify.java
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/examinemenu/LetterIdentify.java
@@ -1,8 +1,15 @@
 package fhss.sjp.thesis.dyslexiaapp.examinemenu;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import fhss.sjp.thesis.dyslexiaapp.R;
 
 public class LetterIdentify extends AppCompatActivity {
 
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
 }
