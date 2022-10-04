1673c1b48d0aca7c52a62791c875ed08db468315
Mahesh-Abeykoon <maheshoabeykoon@gmail.com>
2022-10-04 14:41:51 +0530

    Create Word Pronunciation Screen

---
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/speakingwriting/WordPronunciationScreen.java
@@ -0,0 +1,18 @@
+package fhss.sjp.thesis.dyslexiaapp.speakingwriting;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fhss.sjp.thesis.dyslexiaapp.R;
+
+public class WordPronunciationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.word_pronunciation_screen);
+    }
+
+
+}
--- app/src/main/java/fhss/sjp/thesis/dyslexiaapp/speakingwriting/WriteAndSpeakScreen.java
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/speakingwriting/WriteAndSpeakScreen.java
@@ -1,11 +1,14 @@
 package fhss.sjp.thesis.dyslexiaapp.speakingwriting;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import fhss.sjp.thesis.dyslexiaapp.R;
+import fhss.sjp.thesis.dyslexiaapp.learningmenu.LearnColors;
 
 
 public class WriteAndSpeakScreen extends AppCompatActivity {
@@ -14,6 +17,11 @@ public class WriteAndSpeakScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.write_speak_screen);
+    }
+
+    public void openWordPronunciationScreen(View v){
+        Intent intent = new Intent(this, WordPronunciationScreen.class);
+        startActivity(intent);
     }
 
 
