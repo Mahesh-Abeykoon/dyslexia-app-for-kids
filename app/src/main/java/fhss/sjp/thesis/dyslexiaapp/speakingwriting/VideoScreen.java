35580d850f5b8cec2192507baf2de79d312008b4
Mahesh-Abeykoon <maheshoabeykoon@gmail.com>
2022-10-06 19:30:00 +0530

    Create video screen java class

---
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/speakingwriting/VideoScreen.java
@@ -0,0 +1,20 @@
+package fhss.sjp.thesis.dyslexiaapp.speakingwriting;
+
+import androidx.appcompat.app.AppCompatActivity;
+import fhss.sjp.thesis.dyslexiaapp.R;
+import android.os.Bundle;
+
+
+public class VideoScreen extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vedio_screen);
+
+       
+
+    }
+}
