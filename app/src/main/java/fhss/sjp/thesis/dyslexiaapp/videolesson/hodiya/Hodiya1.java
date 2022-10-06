d9112c7ff199c1b6f0d6bd38a64d2d9f6298d26e
Mahesh-Abeykoon <maheshoabeykoon@gmail.com>
2022-10-06 22:11:09 +0530

    Create Sinhala Hodiya Screen

---
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/videolesson/hodiya/Hodiya1.java
@@ -0,0 +1,5 @@
+package fhss.sjp.thesis.dyslexiaapp.videolesson.hodiya;
+
+public class Hodiya1 {
+
+}
---
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/videolesson/hodiya/HodiyaScreen.java
@@ -0,0 +1,22 @@
+package fhss.sjp.thesis.dyslexiaapp.videolesson.hodiya;
+
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fhss.sjp.thesis.dyslexiaapp.R;
+
+public class HodiyaScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hodiya_screen);
+    }
+
+
+
+
+}
