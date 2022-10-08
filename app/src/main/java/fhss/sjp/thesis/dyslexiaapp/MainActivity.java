3a3573191af4cf3510aea17db6fe434c007bf08f
Mahesh-Abeykoon <maheshoabeykoon@gmail.com>
2022-10-09 00:26:08 +0530

    Add splash screen

--- app/src/main/java/fhss/sjp/thesis/dyslexiaapp/MainActivity.java
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        setTheme(R.style.SplashScreenTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
