f8db8ac8bba896f762bae175226e4e12f29db5aa
Mahesh-Abeykoon <maheshoabeykoon@gmail.com>
2022-10-09 21:02:41 +0530

    Connect new screen

--- app/src/main/java/fhss/sjp/thesis/dyslexiaapp/videolesson/MiniDictionaryScreen.java
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/videolesson/MiniDictionaryScreen.java
@@ -1,5 +1,17 @@
 package fhss.sjp.thesis.dyslexiaapp.videolesson;
 
-public class MiniDictionaryScreen {
-    
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fhss.sjp.thesis.dyslexiaapp.R;
+
+public class MiniDictionaryScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_lesson_screen);
+    }
+
+
 }
