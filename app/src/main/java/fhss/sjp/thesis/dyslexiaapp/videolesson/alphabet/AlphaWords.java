7416dc41902e874b674c65d32e92c10d7bc68667
Mahesh-Abeykoon <maheshoabeykoon@gmail.com>
2022-10-11 17:36:54 +0530

    Create Alphabet learning screen

---
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/videolesson/alphabet/AlphaWords.java
@@ -0,0 +1,4 @@
+package fhss.sjp.thesis.dyslexiaapp.videolesson.alphabet;
+
+public class AlphaWords {
+}
--- app/src/main/java/fhss/sjp/thesis/dyslexiaapp/videolesson/alphabet/EngAlpScreen.java
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/videolesson/alphabet/EngAlpScreen.java
@@ -1,16 +1,23 @@
 package fhss.sjp.thesis.dyslexiaapp.videolesson.alphabet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import fhss.sjp.thesis.dyslexiaapp.R;
+
 
 public class EngAlpScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.eng_alp_screen);
+    }
+    public void openAplhaScreen(View v){
+        Intent intent = new Intent(this, AlphaWords.class);
+        startActivity(intent);
     }
 
 
