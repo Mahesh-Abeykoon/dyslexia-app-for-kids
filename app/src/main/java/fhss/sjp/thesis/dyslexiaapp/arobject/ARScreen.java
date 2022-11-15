8fe4ae64b8d85092ca166cd913d6ce6f32a4e34e
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2022-11-15 23:32:28 +0530

    Create onCreate method

--- app/src/main/java/fhss/sjp/thesis/dyslexiaapp/arobject/ARScreen.java
+++ app/src/main/java/fhss/sjp/thesis/dyslexiaapp/arobject/ARScreen.java
@@ -1,4 +1,42 @@
 package fhss.sjp.thesis.dyslexiaapp.arobject;
 
-public class ARScreen {
-}
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ux.ArFragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ARScreen extends AppCompatActivity {
+
+    private ArFragment arFragment;
+    private Button btnRemove;
+    AnchorNode anchorNode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ar_screen);
+
+        arFragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.fragment);
+        removeButton = (Button)findViewById(R.id.remove);
+        getImages();
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+
+            Anchor anchor = hitResult.createAnchor();
+
+            ModelRenderable.builder()
+                    .setSource(this,Uri.parse(Common.model))
+                    .build()
+                    .thenAccept(modelRenderable -> addModelToScene(anchor,modelRenderable));
+        });
+
+        removeButton.setOnClickListener(view -> removeAnchorNode(anchorNode));
+    }
+
+
+    }
+
